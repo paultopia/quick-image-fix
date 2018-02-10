470950b825cfe5144baa27ccddc21fb34f952509
Paul Gowder <paul.gowder@gmail.com>
2018-02-10 00:22:35 -0600

    cleanup

--- webfix.js
+++ webfix.js
@@ -2,22 +2,15 @@ document.getElementById('infile').onchange = function(e) {
     loadImage(
         e.target.files[0],
         function(canvas) {
-            console.log(canvas);
             canvas.toBlob(function(blob){
-                var newImg = document.createElement('img');
                 var url = URL.createObjectURL(blob);
-                // newImg.onload = function() {
-                //     URL.revokeObjectURL(url);
-                // };
                 var downLink = document.createElement('a');
                 downLink.setAttribute('href', url);
                 downLink.src = url;
                 downLink.innerHTML = "download";
                 downLink.download="fixed.jpg";
                 document.body.appendChild(downLink);
-                document.body.appendChild(newImg);
             }, 'image/jpeg', .95);
-            //document.body.appendChild(canvas);
         },
         {orientation: true}
     );
